1863f5a4f3be6940ee7d8a693b83d8284ff05d2f
Mohit Handa <mks@Mohits-Air.home>
2024-01-22 19:21:28 +0100

    adding enums class

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.nl.banking.mortgage.mortgagecalculator"}];updateSearchResults();
---
+++ src/main/java/com/nl/banking/mortgage/mortgagecalculator/enums/Constants.java
@@ -0,0 +1,5 @@
+package com.nl.banking.mortgage.mortgagecalculator.enums;
+
+public class Constants {
+
+}
