7c0d5fe3e80c199289c5fb81ee760aeccc57ade0
mohithanda03 <56130855+mohithanda03@users.noreply.github.com>
2024-01-22 19:32:29 +0100

    Update MortgageCheckResponse.java

--- src/main/java/com/nl/banking/mortgage/mortgagecalculator/model/MortgageCheckResponse.java
+++ src/main/java/com/nl/banking/mortgage/mortgagecalculator/model/MortgageCheckResponse.java
@@ -1,5 +1,19 @@
 package com.nl.banking.mortgage.mortgagecalculator.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Setter
+@Getter
 public class MortgageCheckResponse {
 
+    private boolean feasible;
+    private double monthlyCosts;
+
+    public MortgageCheckResponse(boolean feasible, double monthlyCosts) {
+        this.feasible = feasible;
+        this.monthlyCosts = monthlyCosts;
+    }
 }
