8cb8cf7f70c4b1669108a52bf60c5d9ea424170b
mohithanda03 <56130855+mohithanda03@users.noreply.github.com>
2024-01-22 19:31:21 +0100

    Update MortgageServiceImpl.java

--- src/main/java/com/nl/banking/mortgage/mortgagecalculator/service/impl/MortgageServiceImpl.java
+++ src/main/java/com/nl/banking/mortgage/mortgagecalculator/service/impl/MortgageServiceImpl.java
@@ -1,5 +1,32 @@
 package com.nl.banking.mortgage.mortgagecalculator.service.impl;
 
-public class MortgageServiceImpl {
+import com.example.mortgage.calculator.model.MortgageCheckRequest;
+import com.example.mortgage.calculator.model.MortgageCheckResponse;
+import com.example.mortgage.calculator.service.InterestRateService;
+import com.example.mortgage.calculator.service.MortgageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import static com.example.mortgage.calculator.utils.Calculations.calculateMonthlyCosts;
+import static com.example.mortgage.calculator.utils.Calculations.isFeasible;
+
+@Service
+public class MortgageServiceImpl implements MortgageService {
+
+    @Autowired
+    private InterestRateService interestRateService;
+
+    @Override
+    public MortgageCheckResponse performMortgageCheck(MortgageCheckRequest request) {
+        double interestRate = interestRateService.getInterestRateByMaturityPeriod(request.getMaturityPeriod());
+
+        boolean feasible = isFeasible(request);
+        double monthlyCosts = calculateMonthlyCosts(request, interestRate);
+
+        return new MortgageCheckResponse(feasible, monthlyCosts);
+    }
+
+
+
 
 }
