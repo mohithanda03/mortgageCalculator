3e975152517c7d6b5820169b13d05becdfb48d66
mohithanda03 <56130855+mohithanda03@users.noreply.github.com>
2024-01-22 19:32:43 +0100

    Update MortgageCheckRequest.java

--- src/main/java/com/nl/banking/mortgage/mortgagecalculator/model/MortgageCheckRequest.java
+++ src/main/java/com/nl/banking/mortgage/mortgagecalculator/model/MortgageCheckRequest.java
@@ -1,5 +1,27 @@
 package com.nl.banking.mortgage.mortgagecalculator.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Setter
+@Getter
 public class MortgageCheckRequest {
 
+    private double income;
+    private int maturityPeriod;
+    private double loanValue;
+    private double homeValue;
+
+    /**/
+
+    public MortgageCheckRequest(double income, int maturityPeriod, double loanValue, double homeValue) {
+        this.income = income;
+        this.maturityPeriod = maturityPeriod;
+        this.loanValue = loanValue;
+        this.homeValue = homeValue;
+    }
+
 }
+
