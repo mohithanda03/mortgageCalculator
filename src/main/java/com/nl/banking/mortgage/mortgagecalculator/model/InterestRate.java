f85b33b63e118dfce483b5af662967a89b1efdf5
mohithanda03 <56130855+mohithanda03@users.noreply.github.com>
2024-01-22 19:32:55 +0100

    Update InterestRate.java

--- src/main/java/com/nl/banking/mortgage/mortgagecalculator/model/InterestRate.java
+++ src/main/java/com/nl/banking/mortgage/mortgagecalculator/model/InterestRate.java
@@ -1,5 +1,21 @@
 package com.nl.banking.mortgage.mortgagecalculator.model;
 
-public class InterestRate {
+import lombok.Getter;
+import lombok.Setter;
 
+import java.sql.Timestamp;
+
+@Setter
+@Getter
+public class InterestRate {
+    private int maturityPeriod;
+    private double interestRate;
+    private Timestamp lastUpdate;
+
+    public InterestRate(int maturityPeriod, double interestRate, Timestamp lastUpdate) {
+        this.maturityPeriod = maturityPeriod;
+        this.interestRate = interestRate;
+        this.lastUpdate = lastUpdate;
+    }
 }
+
