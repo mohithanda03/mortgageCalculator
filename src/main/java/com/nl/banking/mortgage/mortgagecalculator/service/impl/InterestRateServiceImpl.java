8b06a8f88aaa18f2d7e9d541687ecd896fa06825
mohithanda03 <56130855+mohithanda03@users.noreply.github.com>
2024-01-22 19:31:39 +0100

    Update InterestRateServiceImpl.java

--- src/main/java/com/nl/banking/mortgage/mortgagecalculator/service/impl/InterestRateServiceImpl.java
+++ src/main/java/com/nl/banking/mortgage/mortgagecalculator/service/impl/InterestRateServiceImpl.java
@@ -1,5 +1,26 @@
 package com.nl.banking.mortgage.mortgagecalculator.service.impl;
 
-public class InterestRateServiceImpl {
+import com.example.mortgage.calculator.model.InterestRate;
+import com.example.mortgage.calculator.service.InterestRateService;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+import static com.example.mortgage.calculator.MortgageCalculatorApplication.INTEREST_RATES;
+
+@Service
+public class InterestRateServiceImpl implements InterestRateService {
+
+    @Override
+    public List<InterestRate> getCurrentInterestRates() {
+        return INTEREST_RATES;
+    }
+
+    @Override
+    public double getInterestRateByMaturityPeriod(int maturityPeriod) {
+        double interestRate = 0;
+        interestRate = INTEREST_RATES.get(maturityPeriod).getInterestRate();
+        return interestRate;
+    }
 
 }
+
