d94ec4f8461e0bf76d639c34a301dd3e693896b0
mohithanda03 <56130855+mohithanda03@users.noreply.github.com>
2024-01-23 10:47:19 +0100

    Update InterestRateServiceImpl.java

--- src/main/java/com/nl/banking/mortgage/mortgagecalculator/service/impl/InterestRateServiceImpl.java
+++ src/main/java/com/nl/banking/mortgage/mortgagecalculator/service/impl/InterestRateServiceImpl.java
@@ -1,26 +1,29 @@
-package com.nl.banking.mortgage.mortgagecalculator.service.impl;
+package com.nl.payments.mortgagecalculator.service.impl;
 
-import com.example.mortgage.calculator.model.InterestRate;
-import com.example.mortgage.calculator.service.InterestRateService;
+import com.nl.payments.mortgagecalculator.MortgageCalculatorApplication;
+import com.nl.payments.mortgagecalculator.model.InterestRate;
+import com.nl.payments.mortgagecalculator.service.InterestRateService;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
-import static com.example.mortgage.calculator.MortgageCalculatorApplication.INTEREST_RATES;
-
+/**
+ * @author Mohit
+ * Type InterestRateServiceImpl Implements  the InterestRateService.
+ */
 @Service
 public class InterestRateServiceImpl implements InterestRateService {
 
     @Override
     public List<InterestRate> getCurrentInterestRates() {
-        return INTEREST_RATES;
+        return MortgageCalculatorApplication.INTEREST_RATES;
     }
 
     @Override
     public double getInterestRateByMaturityPeriod(int maturityPeriod) {
         double interestRate = 0;
-        interestRate = INTEREST_RATES.get(maturityPeriod).getInterestRate();
+        interestRate = MortgageCalculatorApplication.INTEREST_RATES.get(maturityPeriod - 1).getInterestRatePercentage();
         return interestRate;
     }
 
 }
-
