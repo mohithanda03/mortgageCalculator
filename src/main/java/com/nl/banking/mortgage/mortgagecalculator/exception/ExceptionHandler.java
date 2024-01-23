87cf942f7723aceaa5a9b37df840f9a8e93a0a68
mohithanda03 <56130855+mohithanda03@users.noreply.github.com>
2024-01-23 10:49:20 +0100

    Update and rename ExceptionHandler.java to GlobalExceptionHandler.java

--- src/main/java/com/nl/banking/mortgage/mortgagecalculator/exception/ExceptionHandler.java
+++
@@ -1,5 +0,0 @@
-package com.nl.banking.mortgage.mortgagecalculator.exception;
-
-public class ExceptionHandler {
-
-}
---
+++ src/main/java/com/nl/banking/mortgage/mortgagecalculator/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.nl.payments.mortgagecalculator.exception;
+
+import com.nl.payments.mortgagecalculator.model.ErrorDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+/**
+ * @author Mohit
+ * Type GlobalExceptionHandler helps in returning the custom mesages in case of exceptions to users.
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleGlobalException(Exception exception,
+                                                              WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(),
+                webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
