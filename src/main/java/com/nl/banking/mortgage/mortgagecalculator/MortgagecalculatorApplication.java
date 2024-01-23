a36a7979ffd0318e5e6f04989e1a4ff9a8d0d6fd
mohithanda03 <56130855+mohithanda03@users.noreply.github.com>
2024-01-23 10:44:40 +0100

    Update MortgagecalculatorApplication.java

--- src/main/java/com/nl/banking/mortgage/mortgagecalculator/MortgagecalculatorApplication.java
+++ src/main/java/com/nl/banking/mortgage/mortgagecalculator/MortgagecalculatorApplication.java
@@ -1,6 +1,6 @@
-package com.nl.banking.mortgage.mortgagecalculator;
+package com.nl.payments.mortgagecalculator;
 
-import com.example.mortgage.calculator.model.InterestRate;
+import com.nl.payments.mortgagecalculator.model.InterestRate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,27 +12,37 @@ import java.util.List;
 @SpringBootApplication
 public class MortgageCalculatorApplication {
 
-	public static final List<InterestRate> INTEREST_RATES = new ArrayList<>();
+    public static final List<InterestRate> INTEREST_RATES = new ArrayList<>();
 
-	static {
-		INTEREST_RATES.add(new InterestRate(1, 3.5, Timestamp.from(Instant.now())));
-		INTEREST_RATES.add(new InterestRate(2, 3.2, Timestamp.from(Instant.now())));
-		INTEREST_RATES.add(new InterestRate(3, 3.0, Timestamp.from(Instant.now())));
-		INTEREST_RATES.add(new InterestRate(4, 2.8, Timestamp.from(Instant.now())));
-		INTEREST_RATES.add(new InterestRate(5, 2.8, Timestamp.from(Instant.now())));
-		INTEREST_RATES.add(new InterestRate(6, 2.8, Timestamp.from(Instant.now())));
-		INTEREST_RATES.add(new InterestRate(7, 2.8, Timestamp.from(Instant.now())));
-		INTEREST_RATES.add(new InterestRate(8, 2.8, Timestamp.from(Instant.now())));
-		INTEREST_RATES.add(new InterestRate(9, 2.8, Timestamp.from(Instant.now())));
-		INTEREST_RATES.add(new InterestRate(10, 2.8, Timestamp.from(Instant.now())));
-		INTEREST_RATES.add(new InterestRate(11, 2.8, Timestamp.from(Instant.now())));
-		INTEREST_RATES.add(new InterestRate(12, 2.8, Timestamp.from(Instant.now())));
-		INTEREST_RATES.add(new InterestRate(13, 2.8, Timestamp.from(Instant.now())));
-		INTEREST_RATES.add(new InterestRate(14, 2.8, Timestamp.from(Instant.now())));
-		INTEREST_RATES.add(new InterestRate(15, 2.8, Timestamp.from(Instant.now())));
-	}
+    static {
+        INTEREST_RATES.add(new InterestRate(1, 4.5, Timestamp.from(Instant.now())));
+        INTEREST_RATES.add(new InterestRate(2, 4.4, Timestamp.from(Instant.now())));
+        INTEREST_RATES.add(new InterestRate(3, 4.3, Timestamp.from(Instant.now())));
+        INTEREST_RATES.add(new InterestRate(4, 4.2, Timestamp.from(Instant.now())));
+        INTEREST_RATES.add(new InterestRate(5, 4.1, Timestamp.from(Instant.now())));
+        INTEREST_RATES.add(new InterestRate(6, 4.0, Timestamp.from(Instant.now())));
+        INTEREST_RATES.add(new InterestRate(7, 3.9, Timestamp.from(Instant.now())));
+        INTEREST_RATES.add(new InterestRate(8, 3.8, Timestamp.from(Instant.now())));
+        INTEREST_RATES.add(new InterestRate(9, 3.7, Timestamp.from(Instant.now())));
+        INTEREST_RATES.add(new InterestRate(10, 3.6, Timestamp.from(Instant.now())));
+        INTEREST_RATES.add(new InterestRate(11, 3.5, Timestamp.from(Instant.now())));
+        INTEREST_RATES.add(new InterestRate(12, 3.5, Timestamp.from(Instant.now())));
+        INTEREST_RATES.add(new InterestRate(13, 3.5, Timestamp.from(Instant.now())));
+        INTEREST_RATES.add(new InterestRate(14, 3.5, Timestamp.from(Instant.now())));
+        INTEREST_RATES.add(new InterestRate(15, 3.5, Timestamp.from(Instant.now())));
+        INTEREST_RATES.add(new InterestRate(16, 3.2, Timestamp.from(Instant.now())));
+        INTEREST_RATES.add(new InterestRate(17, 3.2, Timestamp.from(Instant.now())));
+        INTEREST_RATES.add(new InterestRate(18, 3.2, Timestamp.from(Instant.now())));
+        INTEREST_RATES.add(new InterestRate(19, 3.2, Timestamp.from(Instant.now())));
+        INTEREST_RATES.add(new InterestRate(20, 3.2, Timestamp.from(Instant.now())));
+        INTEREST_RATES.add(new InterestRate(21, 3.0, Timestamp.from(Instant.now())));
+        INTEREST_RATES.add(new InterestRate(22, 3.0, Timestamp.from(Instant.now())));
+        INTEREST_RATES.add(new InterestRate(23, 3.0, Timestamp.from(Instant.now())));
+        INTEREST_RATES.add(new InterestRate(24, 3.0, Timestamp.from(Instant.now())));
+        INTEREST_RATES.add(new InterestRate(25, 3.0, Timestamp.from(Instant.now())));
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(MortgageCalculatorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MortgageCalculatorApplication.class, args);
+    }
 }
