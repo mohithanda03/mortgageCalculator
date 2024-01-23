7834d60f26d118ad4e4dcdc133d5fd57f277f41f
mohithanda03 <56130855+mohithanda03@users.noreply.github.com>
2024-01-23 10:47:36 +0100

    Update MortgageController.java

--- src/main/java/com/nl/banking/mortgage/mortgagecalculator/resource/MortgageController.java
+++ src/main/java/com/nl/banking/mortgage/mortgagecalculator/resource/MortgageController.java
@@ -1,21 +1,23 @@
-package com.nl.banking.mortgage.mortgagecalculator.resource;
+package com.nl.payments.mortgagecalculator.resource;
 
-import com.example.mortgage.calculator.model.InterestRate;
-import com.example.mortgage.calculator.model.MortgageCheckRequest;
-import com.example.mortgage.calculator.model.MortgageCheckResponse;
-import com.example.mortgage.calculator.service.InterestRateService;
-import com.example.mortgage.calculator.service.MortgageService;
+import com.nl.payments.mortgagecalculator.model.InterestRate;
+import com.nl.payments.mortgagecalculator.model.MortgageCheckRequest;
+import com.nl.payments.mortgagecalculator.model.MortgageCheckResponse;
+import com.nl.payments.mortgagecalculator.service.InterestRateService;
+import com.nl.payments.mortgagecalculator.service.MortgageService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 /**
  * @author Mohit
  * Type Mortgage Controller Exposes interestRate Endpoints.
  */
-@Tag(name="Recipe API", description = "Recipe management APIs")
+@Tag(name = "Mortgage API", description = "Mortgage Calculator API")
 @RestController
 @RequestMapping("/mortgage")
 public class MortgageController {
@@ -26,11 +28,24 @@ public class MortgageController {
     @Autowired
     private MortgageService mortgageService;
 
+    /**
+     * Gets all the interest Rates.
+     *
+     * @return List of interest Rates.
+     */
+    @Operation(summary = "Get All Interest Rates")
     @GetMapping("/interest-rates")
     public List<InterestRate> getInterestRates() {
         return interestRateService.getCurrentInterestRates();
     }
 
+    /**
+     * Creates a new Recipe.
+     *
+     * @param request Calculates monthly costs based on given fields.
+     * @return Response as feasibility and monthly cost.
+     */
+    @Operation(summary = "Mortgage Check")
     @PostMapping("/mortgage-check")
     public MortgageCheckResponse performMortgageCheck(@Validated @RequestBody MortgageCheckRequest request) {
         return mortgageService.performMortgageCheck(request);
