e59e743c10880edf6efc82df940c9d7f43ba77be
mohithanda03 <56130855+mohithanda03@users.noreply.github.com>
2024-01-23 10:46:38 +0100

    Update MortgageService.java

--- src/main/java/com/nl/banking/mortgage/mortgagecalculator/service/MortgageService.java
+++ src/main/java/com/nl/banking/mortgage/mortgagecalculator/service/MortgageService.java
@@ -1,13 +1,16 @@
-package com.nl.banking.mortgage.mortgagecalculator.service;
+package com.nl.payments.mortgagecalculator.service;
 
-import com.example.mortgage.calculator.model.MortgageCheckRequest;
-import com.example.mortgage.calculator.model.MortgageCheckResponse;
+import com.nl.payments.mortgagecalculator.model.MortgageCheckRequest;
+import com.nl.payments.mortgagecalculator.model.MortgageCheckResponse;
 import org.springframework.stereotype.Service;
 
+/**
+ * @author Mohit
+ * Type MortgageService handles operation on the Mortgage Calculation.
+ */
 @Service
 public interface MortgageService {
 
     MortgageCheckResponse performMortgageCheck(MortgageCheckRequest request);
 
 }
-
