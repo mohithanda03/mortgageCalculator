85369f3875b8ba6e79b61cc770b3ed4a8a1fe9e0
mohithanda03 <56130855+mohithanda03@users.noreply.github.com>
2024-01-23 10:49:59 +0100

    Update Constants.java

--- src/main/java/com/nl/banking/mortgage/mortgagecalculator/enums/Constants.java
+++ src/main/java/com/nl/banking/mortgage/mortgagecalculator/enums/Constants.java
@@ -1,12 +1,15 @@
-package com.nl.banking.mortgage.mortgagecalculator.enums;
+package com.nl.payments.mortgagecalculator.enums;
 
 import lombok.experimental.UtilityClass;
 
+/**
+ * @author Mohit
+ * Type Constant class. Exposes constant values.
+ */
 @UtilityClass
-public class Constants
-{
-    public static final Integer MONTHS_IN_A_YEAR =12 ;
-    public static final Integer MAXIMUM_TIMES_INCOME =4 ;
+public class Constants {
+    public static final Integer MONTHS_IN_A_YEAR = 12;
+    public static final Integer MAXIMUM_TIMES_INCOME = 4;
+    public static final Integer PERCENTAGE = 100;
 
 }
-
